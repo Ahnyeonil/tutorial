54aa52234063eb4f550d4999cac35026dd9a888f
yeonil <ahnyi527@gmail.com>
2021-11-03 19:41:19 +0900

    tutorial_part_4

--- tutorial/polls/models.py
+++ tutorial/polls/models.py
@@ -3,6 +3,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 class Question(models.Model):
     # 문자
     question_text = models.CharField(max_length=200)
@@ -14,6 +15,7 @@ class Question(models.Model):
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     # 외래키 - question 참조 // on_delete -> question 삭제시 답변 삭제
